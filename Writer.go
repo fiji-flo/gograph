1e50f5a4761c83a4084238cef09964a023f4cab1
fiji-flo <flome.rz@gmail.com>
2013-03-16 22:48:18 +0100

    fix index

--- Writer.go
+++ Writer.go
@@ -16,7 +16,7 @@ func Write(name string, G Graph) {
     a := "a %d %d %d\n"
 
     w.WriteString(fmt.Sprintf("p cp %d %d\n", G.N, G.M))
-    for i := 0; i < len(G.V); i++ {
+    for i := 1; i < len(G.V); i++ {
         for _, v := range G.V[i].edges {
             w.WriteString(fmt.Sprintf(a, v.from, v.to, v.weight)) 
         }
